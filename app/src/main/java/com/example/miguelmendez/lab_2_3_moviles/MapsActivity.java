2d388d67d1654f96de2ab1661f9de829fd7563ef
Miguel Mendez Rojas <miguel1796@live.com>
2018-05-09 15:08:38 -0600

    Arreglo SedesDBHelper

--- app/src/main/java/com/example/miguelmendez/lab_2_3_moviles/MapsActivity.java
+++ app/src/main/java/com/example/miguelmendez/lab_2_3_moviles/MapsActivity.java
@@ -2,10 +2,13 @@ package com.example.miguelmendez.lab_2_3_moviles;
 
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -25,6 +28,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private static final int LOCATION_REQUEST_CODE = 101;
@@ -32,6 +37,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Marker markerSC, markerCar, markerLi, markerAmon, markerAla,markerPais;
     public  static final  String EXTRA_LATITUD="";
     public  static final  String EXTRA_LONGITUD="";
+
+    SedesDbHelper MyBd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +49,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        SedesDbHelper db = new SedesDbHelper(getApplicationContext());
+        insertarDatos();
+        ArrayList<SedesTEC> arrayList = SedesContract.listaSedes(getApplicationContext());
+    }
 
-        String q = "SELECT * FROM sedes";
-        Cursor mCursor = db.rawQuery(q, null);
+    public void insertarDatos(){
+        SedesContract.insertarSede(getApplicationContext(),
+                new SedesTEC("TEC Sede Santa Clara", 10.3594215,
+                        -84.5133537, "Esta sede, ubicada en Santa Clara de San Carlos, en la región tropical húmeda, se encuentra a 105 kilómetros de San José y a 170 metros sobre el nivel " +
+                        "del mar, con una temperatura media anual de 26ºC."));
+
+        SedesContract.insertarSede(getApplicationContext(),
+                new SedesTEC("TEC Sede Central Cartago", 9.8564963,
+                        -83.9125516, "La Sede Central del Tecnológico de Costa Rica se encuentra en Cartago, una ciudad que se ubica 24 kilómetros al sureste de San José," +
+                        " a una altitud de 1,435 metros sobre el nivel del mar y con un clima tropical húmedo, aunque suele ser más templado debido a su ubicación geográfica y altura, " +
+                        "con lluvias moderadas y temperaturas frescas que varían entre 15 y 26 grados centígrados la mayor parte del año."));
+
+        SedesContract.insertarSede(getApplicationContext(),
+                new SedesTEC("Centro Académico Limón", 9.8400319,
+                    -83.6980679, "La provincia de Limón se encuentra en un momento de expansión producto de la inversión pública y privada. La región Huetar Caribe contará" +
+                    " con obras de infraestructura que apuntan hacia un desarrollo social, entre ellas: la Megaterminal APM, la nueva refinadora de Recope, la Planta Hidroeléctrica del Reventazón y la ampliación de la ruta 32."));
+
+        SedesContract.insertarSede(getApplicationContext(),
+                new SedesTEC("Centro Académico San José", 9.9380554,
+                -84.0775682, "El Centro Académico de San José se ubica en Barrio Amón, entre calles 5 y 7 y entre avenidas 9 y 11.  Sus aulas, oficinas administrativas, biblioteca y áreas recreativas se distribuyen "+
+                "entre edificios nuevos y casas antiguas con el valor histórico propio del barrio."));
+
+        SedesContract.insertarSede(getApplicationContext(),new SedesTEC("Centro académico Alajuela", 10.0198805,
+                -84.1994593, "El Centro Académico se ubica en Desamparados de Alajuela, a 21 kilómetros de San José. Se trata de una localidad en la región intertropical, " +
+                "a 960 metros sobre el nivel del mar y con una temperatura media anual de 22,3ºC. " +
+                "La precipitación pluvial media es de 2.069 mm. anuales, con una estación seca de enero a mediados de marzo y una estación lluviosa de mediados de marzo a diciembre."));
 
     }
 
--- app/src/main/java/com/example/miguelmendez/lab_2_3_moviles/SedesContract.java
+++ app/src/main/java/com/example/miguelmendez/lab_2_3_moviles/SedesContract.java
@@ -1,13 +1,52 @@
 package com.example.miguelmendez.lab_2_3_moviles;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
-public class SedesContract {
-    public static abstract class SedesEntry implements BaseColumns {
-        public static final String TABLE_NAME ="sedes";
-        public static final String nombre = "nombre";
-        public static final String latitud = "latitud";
-        public static final String longitud = "longitud";
-        public static final String descripcion = "descripcion";
+import java.util.ArrayList;
+
+public class SedesContract implements BaseColumns {
+
+    public static String TABLE_NAME ="sedes.db";
+    public static String nombre = "nombre";
+    public static String latitud = "latitud";
+    public static String longitud = "longitud";
+    public static String descripcion = "descripcion";
+
+
+    public static long insertarSede(Context context, SedesTEC sede){
+        SedesDbHelper dbHelper  = new SedesDbHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues value = new ContentValues();
+        value.put(nombre, sede.getNombre());
+        value.put(latitud,sede.getLatitud());
+        value.put(longitud,sede.getLongitud());
+        value.put(descripcion,sede.getLongitud());
+        return db.insert(TABLE_NAME,null,value);
     }
+
+    public static ArrayList<SedesTEC> listaSedes(Context context){
+        String  query  =  "SELECT    *  FROM  "  +  TABLE_NAME;
+        SedesDbHelper DatabaseHelper = new SedesDbHelper(context);
+        SQLiteDatabase db = DatabaseHelper.getWritableDatabase();
+        Cursor cursor  = db.rawQuery(query, null);
+        ArrayList<SedesTEC> arrayList  =  new ArrayList<>();
+        if (cursor.moveToFirst()) {
+            do {
+                SedesTEC list = new SedesTEC(
+                        cursor.getString(cursor.getColumnIndexOrThrow(nombre)),
+                        cursor.getDouble(cursor.getColumnIndexOrThrow(latitud)),
+                        cursor.getDouble(cursor.getColumnIndexOrThrow(longitud)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(descripcion)));
+                arrayList.add(list);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return arrayList;
+    }
+
+
 }
